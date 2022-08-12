0b18fa21d4a1eaaa4f3226a26ad7606458bfb49b
丁心雨 <932978673@qq.com>
2022-08-12 14:13:03 +0800

    引入路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+/*
+ * @Descripttion: 
+ * @Author: DXY
+ * @Date: 2022-08-10 13:46:42
+ * @LastEditors: DXY
+ * @LastEditTime: 2022-08-12 14:12:40
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    }
+  }
 })
